ca71496b590bd348c3d38058c18e61276b0cdd40
juniorlira08 <juniorlira08@gmail.com>
2021-09-23 12:11:42 -0300

    adicionando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar color="green">{{ nome }}</v-app-bar>
+      <v-app-bar color="green">{{ firstName }}</v-app-bar>
       <div class="menssages-container">
         <div v-for="(message, index) in messages" :key="index">
-          <v-card outlined class="ma-3" :class="message.name == 'Eu' ? 'purple lighten-2' : 'teal accent-4'">
-            <v-card-subtitle class="font-weight-bold">{{ message.name }}</v-card-subtitle>
-            <v-card-text>{{ message.text }}</v-card-text>
-          </v-card>
+          <message-card :messageProp="message" />
         </div>
       </div>
-      <v-app-bar color="#424242" fixed bottom>
-        <v-text-field @keyup.enter="sendMessage()" v-model="campo_text" solo hide-details label="Enviar mensagem..."></v-text-field>
-        <v-btn @click="sendMessage()" icon color="blue">
-          <v-icon>mdi-send</v-icon>
-        </v-btn>
-      </v-app-bar>
+      <bottom-bar @send-message="addMessage($event)" />
     </v-main>
   </v-app>
 </template>
 
 <script>
+  import MessageCard from './components/MessageCard.vue'
+  import BottomBar from './components/BottomBar.vue'
   export default {
+    components : {
+      MessageCard,
+      BottomBar
+    },
     data() {
       return {
-        nome: "Roberto",
+        nome: "Roberto Silva",
         messages: [
           {
             name: "Roberto Silva",
@@ -35,17 +33,18 @@
             text: "Tudo bem?"
           }
         ],
-        campo_text: ""
+      }
+    },
+    computed : {
+      firstName() {
+        console.log('nhanha')
+        return this.nome.split(' ')[0]
       }
     },
     methods: {
-      sendMessage() {
-        let message = {
-          name: "Eu",
-          text: this.campo_text
-        }
-        this.messages.push(message)
-        this.campo_text = ""
+      addMessage(event) {
+        console.log(event)
+        this.messages.push(event)
       }
     }
   }
---
+++ src/components/BottomBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app-bar color="#424242" fixed bottom>
+     <v-text-field @keyup.enter="send()" v-model="field" solo hide-details label="Enviar mensagem..."></v-text-field>
+        <v-btn @click="send()" icon color="blue">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+           field: ''
+        }
+    },
+    methods: {
+        send() {
+            let message = {
+                name: 'Eu',
+                text: this.field
+            }
+            this.$emit('send-message', message)
+            this.field = ''
+        }
+    }
+}
+</script>
